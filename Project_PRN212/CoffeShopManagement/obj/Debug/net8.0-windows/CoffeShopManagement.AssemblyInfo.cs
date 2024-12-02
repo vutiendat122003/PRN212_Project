e3aae637948d68048bd240aae742f9744769e69b
khaitdhe171989 <trandinhkhai09072003@gmail.com>
2024-12-02 11:29:00 +0700

    test

--- Project_PRN212/CoffeShopManagement/obj/Debug/net8.0-windows/CoffeShopManagement.AssemblyInfo.cs
+++ Project_PRN212/CoffeShopManagement/obj/Debug/net8.0-windows/CoffeShopManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeShopManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c48dfe2963b92aee17f6e8be1db5e739b0c193")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeShopManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeShopManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project_PRN212/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Project_PRN212/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c48dfe2963b92aee17f6e8be1db5e739b0c193")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
