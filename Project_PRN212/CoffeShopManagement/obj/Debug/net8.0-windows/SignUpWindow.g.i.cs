ad2a8d990f899af26850308a7cf2f40265afe0da
khaitdhe171989 <trandinhkhai09072003@gmail.com>
2024-12-09 09:42:35 +0700

    Add ProductPage.xaml

--- Project_PRN212/CoffeShopManagement/obj/Debug/net8.0-windows/SignUpWindow.g.i.cs
+++ Project_PRN212/CoffeShopManagement/obj/Debug/net8.0-windows/SignUpWindow.g.i.cs
@@ -78,13 +78,13 @@ namespace CoffeShopManagement {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CoffeShopManagement;component/signupwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CoffeShopManagement;V1.0.0.0;component/signupwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SignUpWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -94,7 +94,7 @@ namespace CoffeShopManagement {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.11.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
